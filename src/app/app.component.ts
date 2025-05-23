8cfbb23b31965266ebe2a69b9db1837c5672194b
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-05-23 14:17:50 +0300

    bug fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'app/core/auth/auth.service';
 @Component({
     selector   : 'app-root',
     templateUrl: './app.component.html',
     styleUrls  : ['./app.component.scss']
 })
-export class AppComponent
+export class AppComponent implements OnInit
 {
     /**
      * Constructor
      */
-    constructor()
+    constructor(
+        private authService: AuthService
+    )
     {
+    }
+
+    ngOnInit() : void {
+        this.authService.check().subscribe();
     }
 }
--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -99,42 +99,24 @@ export class AuthService
     /**
      * Sign in using the access token
      */
-    signInUsingToken(): Observable<any>
-    {
-        // Sign in using the token
-        return this._httpClient.post('api/auth/sign-in-with-token', {
-            accessToken: this.accessToken
-        }).pipe(
-            catchError(() =>
-
-                // Return false
-                of(false)
-            ),
-            switchMap((response: any) => {
-
-                // Replace the access token with the new one if it's available on
-                // the response object.
-                //
-                // This is an added optional step for better security. Once you sign
-                // in using the token, you should generate a new one on the server
-                // side and attach it to the response object. Then the following
-                // piece of code can replace the token with the refreshed one.
-                if ( response.accessToken )
-                {
-                    this.accessToken = response.accessToken;
-                }
-
-                // Set the authenticated flag to true
-                this._authenticated = true;
-
-                // Store the user on the user service
-                this._userService.user = response.user;
-
-                // Return true
-                return of(true);
-            })
-        );
+    signInUsingToken(): Observable<any> {
+    const token = this.accessToken;
+    if (!token) {
+        return throwError(() => new Error('No token found'));
     }
+    const payload = AuthUtils['_decodeToken'](token);
+    const role = payload["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
+    const userWithRole = {
+        id: payload.Id,
+        name: payload.Name,
+        email: payload.Email,
+        userName: payload.UserName,
+        roles: role ? [role] : [],
+        status: 'active'
+    };
+    this._userService.user = userWithRole;
+    return of(userWithRole);
+}
 
     /**
      * Sign out
